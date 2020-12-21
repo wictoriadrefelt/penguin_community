d786bc8aa23d948508cd6f3164776856521787f9
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-21 15:04:12 +0100

    Reorganized for use of bootstrap

--- app.py
+++ app.py
@@ -6,11 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_index():
-    return render_template('index.html')
+    return render_template('base.html')
 
-@app.route('/login.html')
-def get_login():
-    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
