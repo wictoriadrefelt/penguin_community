644edaf7b290cb52b91d89289470c05ad466175d
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-30 10:54:06 +0100

    Added url_for for nav

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,12 +8,12 @@ def get_index():
     return render_template('index.html')
 
 
-@app.route('/feed.html')
+@app.route('/feed')
 def get_feed():
     return render_template('feed.html')
 
 
-@app.route('/profile.html')
+@app.route('/profile')
 def get_profile():
     return render_template('profile.html')
 
