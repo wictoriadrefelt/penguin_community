fed2647d14cd1de1add8768d8df00b8f445729cc
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-30 14:49:02 +0100

    Fixed merged-error

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
-
 from flask import Flask, render_template, url_for
-
-from flask import Flask, render_template
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config["SECRET KEY"] = ""
+
 
 @app.route('/')
 def get_index():
     return render_template('index.html')
+
+
 @app.route('/feed')
-
-
-
-@app.route('/feed.html')
 def get_feed():
     return render_template('feed.html')
 
 
-
 @app.route('/profile')
-@app.route('/test_profile.html')
 def get_profile():
+    return render_template('profile.html')
+
+
+@app.route('/test_profile')
+@app.route('/test_profile.html')
+def get_test_profile():
     return render_template('test_profile.html')
 
 
