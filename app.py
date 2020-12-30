428dc0b0ffac882811e5a8696d1b813592b3f5a7
Wictoria Drefelt <wictoria.soderlunddrefeldt@gmail.com>
2020-12-30 10:40:46 +0100

    "stashed away"

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config["SECRET KEY"] = ""
 
 @app.route('/')
 def get_index():
     return render_template('index.html')
+
+
 
 
 @app.route('/feed.html')
@@ -13,9 +16,9 @@ def get_feed():
     return render_template('feed.html')
 
 
-@app.route('/profile.html')
+@app.route('/test_profile.html')
 def get_profile():
-    return render_template('profile.html')
+    return render_template('test_profile.html')
 
 
 if __name__ == '__main__':
