53c8f6e69b1e165511d1a602b6adb97d6580cefe
Wictoria Drefelt <wictoria.soderlunddrefeldt@gmail.com>
2021-01-12 09:14:03 +0100

    "Added password hashing"

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecret"
 
+bcrypt = Bcrypt(app)
 
 from app import routes
--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, bcrypt
 from data import db
 from flask import render_template, request, redirect
 from controllers.web_controller import create_new_user
@@ -30,12 +30,12 @@ def get_sign_up():
 def post_sign_up():
     form = RegistrationForm()
     if form.validate_on_submit():
-        #hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
         first_name = form.first_name.data
         last_name = form.last_name.data
         email = form.email.data
         password = form.password.data
-        create_new_user(first_name, last_name, email, password)
+        create_new_user(first_name, last_name, email, hashed_password)
     return render_template('sign_up.html', form=form)
 
 
