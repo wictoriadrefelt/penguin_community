f520388cbe79f66b1f02de17af58dcd39fac5afd
Wictoria Drefelt <wictoria.soderlunddrefeldt@gmail.com>
2021-01-13 13:16:43 +0100

    "fixed error messages, fixed email_validator"

--- Flask/data/forms.py
+++ Flask/data/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, DateTimeField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, email_validator, Email
 
 
 class RegistrationForm(FlaskForm):
     first_name = StringField("First Name", validators=[DataRequired(), Length(min=2, max=30)])
     last_name = StringField("Last Name", validators=[DataRequired(), Length(min=2, max=30)])
-    email = StringField("Email", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Sign up")
