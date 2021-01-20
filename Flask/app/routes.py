20eaea6fed6acfb4604b304a959f3cacd9c756c8
Victor Olofsson <victorolofsson@hotmail.com>
2021-01-20 19:36:52 +0100

    Added errorpages

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -101,9 +101,19 @@ def logged_out():
     return render_template('logged_out.html')
 
 
+@app.errorhandler(403)
+def error_403(error):
+    return render_template('errors/403.html'), 403
 
 
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html'), 404
 
+
+@app.errorhandler(500)
+def error_500(error):
+    return render_template('errors/500.html'), 500
 
 """  form = PostForm()
     if form.validate_on_submit():
