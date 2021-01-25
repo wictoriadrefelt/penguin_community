f3ea435e0850eb18b24ff215ef30d7a495c46d79
Victor Olofsson <victorolofsson@hotmail.com>
2021-01-25 20:27:36 +0100

    Moved functionality from route->controller

--- Flask/controllers/web_controller.py
+++ Flask/controllers/web_controller.py
@@ -7,7 +7,6 @@ from bson import ObjectId
 #    return wr.create_new_post(location, fishes, image, description, time_stamp)
 
 
-
 def get_user_by_email(email):
     return wr.get_user_by_email(email)
 
@@ -25,15 +24,22 @@ def get_all_posts():
     return wr.get_all_posts()
 
 
-#def add_comment_to_post(post_id, comment_id):
+# def add_comment_to_post(post_id, comment_id):
 #    return wr.add_comment_to_post(post_id, comment_id)
 
 
-#def create_new_comment(user_id, first_name, last_name, text, time_stamp):
+# def create_new_comment(user_id, first_name, last_name, text, time_stamp):
 #    return wr.create_new_comment(user_id, first_name, last_name, text, time_stamp)
 
 """def like_post():
     return"""
 
-def get_users_by_first_name(first_name):
-    return wr.get_users_by_first_name(first_name)
+def get_users_by_first_or_last_name(search_input):
+    users_first = wr.get_users_by_first_name(search_input)
+    users_last = wr.get_users_by_last_name(search_input)
+    users = set(users_first) | set(users_last)
+
+    return [
+        {"first name": user.first_name.capitalize(),
+         "last name": user.last_name.capitalize()}
+        for user in users]
