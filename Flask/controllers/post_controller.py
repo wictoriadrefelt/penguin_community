d96724886dfc0fe54f5dbcf35287f10e16dba468
Victor Olofsson <victorolofsson@hotmail.com>
2021-02-07 20:14:25 +0100

    added get_milliseconds_since_epoch

--- Flask/controllers/post_controller.py
+++ Flask/controllers/post_controller.py
@@ -1,6 +1,6 @@
 import data.repositories.post_repo as pr
 import data.repositories.web_repo as wr
-from controllers.controller_functions import convert_datetime_to_str, decode_picture
+from controllers.controller_functions import decode_picture, get_milliseconds_since_epoch
 
 
 def get_posts_paginate(page_num=1, items_per_page=3):
@@ -9,7 +9,7 @@ def get_posts_paginate(page_num=1, items_per_page=3):
     return [{"first_name": post.user.first_name,
              "last_name": post.user.last_name,
              "description": post.description,
-             "post_date": convert_datetime_to_str(post.post_date),
+             "ms_since_post_date": get_milliseconds_since_epoch(post.post_date),
              "photo": decode_picture(post.photo),
              "profile_picture": decode_picture(post.user.profile_picture),
              "fishes": wr.number_of_fishes_on_post(post.id),
